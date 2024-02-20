28ed5f94e378445437cdb3e77ae0ff33ecc895ec
Tarun S <tarun.sai@atyati.com>
2024-02-21 02:08:52 +0530

    local db code added

---
+++ app/src/main/java/com/teach/lmten/feature_todo/data/local/TodoDatabase.kt
@@ -0,0 +1,15 @@
+package com.teach.lmten.feature_todo.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.teach.lmten.feature_todo.data.local.dto.LocalTodoItem
+
+@Database(entities = [LocalTodoItem::class],
+    version = 1,
+    exportSchema = false)
+abstract class TodoDatabase:RoomDatabase() {
+    abstract val dao:TodoDao
+    companion object{
+        const val DATABASE_NAME="todo_db"
+    }
+}
